ca320c410e2a62fa96f23ca4a91e0c9c600cae98
Michael Barends <michael@unframed.nl>
2025-01-09 09:58:44 +0100

    created customer table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,23 @@
 import { Metadata } from "next";
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
     title: 'Customers',
 }
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>;
+}) {
 
-export default function Page() {
-    return <p>Customers Page</p>;
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <Table customers={customers} />
+        </div>
+    );
 }
